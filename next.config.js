eba993f6ce2a933751316e68394ec97ac7096c5a
joaodeltai <deltaitecnologia@gmail.com>
2025-02-01 01:04:05 -0300

    Fix: Remove deprecated target property from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: 'serverless',
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
