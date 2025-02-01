5c97304e59039e9037dc479883369479e9451c4e
joaodeltai <deltaitecnologia@gmail.com>
2025-02-01 01:23:10 -0300

    Fix: Simplify Netlify and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/api/:path*',
-          destination: '/api/:path*',
-        },
-      ],
-    };
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
